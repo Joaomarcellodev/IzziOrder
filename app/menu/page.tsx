2fde681611d4e4f0270ed11f578c2fb3808cc07b
Tiago Freitas <tiagohuehuehue@gmail.com>
2025-09-13 12:56:54 -0300

    🚧 : first configuration related to supabase

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,12 @@
 import { AppShell } from "@/components/organisms/app-shell";
 import { MenuManagement } from "@/components/organisms/menu-management";
+import { createClient } from "@/lib/supabase/server";
 
-export default function MenuPage() {
+export default async function MenuPage() {
+  const supabaseClient = createClient()
+
+  const { data: menuItems } = await (await supabaseClient).from("menu_items").select()
+
   return (
     <AppShell
       currentPage="Gerenciamento de cardápio"
