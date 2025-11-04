fee5a59b5bdc7138386a1ad13acc8e3d5172aa5b
Joaomarcellodev <joaomarcellobraz@gmail.com>
2025-11-04 18:09:25 -0300

    feat: add toast and change checkbox permissions

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -9,6 +9,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
+      <head>
+        <link rel="icon" href="favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" href="favicon-32x32.png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="manifest" href="favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   )
