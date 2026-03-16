9b6c96fbbef22ae7512ffe2ae8ff5a0716e281c2
tiagohue <tiagohuehuehue@gmail.com>
2026-03-16 17:25:05 -0300

    refactor: sign up refactor

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
+import { User } from '@/lib/user'
 
 export async function login(formData: FormData) {
     const supabase = await createClient()
@@ -31,11 +32,11 @@ export async function login(formData: FormData) {
 export async function signup(formData: FormData) {
     const supabase = await createClient()
 
-    // type-casting here for convenience
-    // in practice, you should validate your inputs
+    const user = User.fromFormData(formData)
+
     const data = {
-        email: formData.get('email') as string,
-        password: formData.get('password') as string,
+        email: user.email,
+        password: user.password,
     }
 
     const { error } = await supabase.auth.signUp(data)
