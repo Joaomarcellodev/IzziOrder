7374badc5b26fe53eb887b034ca41a344c1a38de
tiagohue <tiagohuehuehue@gmail.com>
2026-03-21 12:10:52 -0300

    feat: created user-actions

---
+++ app/actions/user-actions.ts
@@ -0,0 +1,28 @@
+"use server"
+
+import { User } from "@/lib/entities/user";
+import { createClient } from "@/utils/supabase/server"
+import { redirect } from "next/dist/server/api-utils";
+
+export async function getUser() {
+    const supabase = await createClient()
+    const user = await getUserAuthenticated(supabase)
+    const name = await getUserName(supabase, user)
+    return new User(name, user.email)
+}
+
+export async function getUserAuthenticated(supabase: any) {
+    const { data: { user }, error: authError } = await supabase.auth.getUser();
+    if (authError || !user) {
+        throw new Error("User not authenticated");
+    }
+    return user;
+}
+
+export async function getUserName(supabase: any, user: any) {
+    const { data, error } = await supabase.from("profiles").select("user_name").eq("id", user.id).single();
+    if (error) {
+        throw new Error(error.message)
+    }
+    return data.user_name;
+}
