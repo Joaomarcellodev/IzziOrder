6bf8671bffa673e7211d3fb89edbe1f44dce6705
tiagohue <tiagohuehuehue@gmail.com>
2026-03-27 12:26:04 -0300

    chore: reconfigured playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,12 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+dotenv.config({ path: path.resolve(__dirname, '.env.test-e2e'), override: true });
 
 export default defineConfig({
   timeout: 60000,
@@ -37,32 +45,16 @@ export default defineConfig({
 
       },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'NODE_ENV=test-e2e npm run dev',
+    command: 'npm run dev',
     port: 3000,
     reuseExistingServer: true,
+    env: {
+      ...process.env,
+      NODE_ENV: 'test-e2e',
+    },
   },
 });
