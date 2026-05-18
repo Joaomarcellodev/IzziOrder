67b7b3b8d1164a22fb3e2fce512108a864e72994
olimpio7 <kiodacarvalho@gmail.com>
2026-05-17 22:16:42 -0300

    refactor: unified cash payments in report processor

--- lib/entities/sales-report-processor.ts
+++ lib/entities/sales-report-processor.ts
@@ -145,8 +145,9 @@ export class SalesReportProcessor {
 
         // Sales by Payment Method
         if (order.raw.payment_method) {
-            const current = acc.salesByPaymentMethod.get(order.raw.payment_method) || 0;
-            acc.salesByPaymentMethod.set(order.raw.payment_method, current + order.effectiveRevenue);
+            const method = order.raw.payment_method.includes("ESPECIE") ? "DINHEIRO" : order.raw.payment_method;
+            const current = acc.salesByPaymentMethod.get(method as PaymentMethod) || 0;
+            acc.salesByPaymentMethod.set(method as PaymentMethod, current + order.effectiveRevenue);
         }
 
         // Orders by Type
